7c4e589712168da296222cdff7e2a70ad2f6e7ef
4865sj <109568436+4865sj@users.noreply.github.com>
2022-08-10 17:23:40 +0900

    Update save.rs

--- save.rs
+++ save.rs
@@ -1,4 +1,4 @@
-use concrete::{ConfigBuilder, generate_keys, set_server_key, FheUint8};
+use concrete::{ConfigBuilder, generate_keys, set_server_key, FheUint16};
 use concrete::{ServerKey, ClientKey};
 use concrete::prelude::*;
 
@@ -32,16 +32,16 @@ fn read_client_key() -> ClientKey {
     de_data
 }
 
-fn save_ciphertext(data: Vec<FheUint8>) {
+fn save_ciphertext(data: Vec<FheUint16>) {
     let mut file = std::io::BufWriter::new(std::fs::File::create("ciphertext.bin").unwrap());
 
     bincode::serialize_into(file, &data).unwrap();
 }
 
-fn read_ciphertext() -> Vec<FheUint8> {
+fn read_ciphertext() -> Vec<FheUint16> {
     let mut file = std::io::BufReader::new(std::fs::File::open("ciphertext.bin").unwrap());
 
-    let de_data: Vec<FheUint8> = bincode::deserialize_from(file).unwrap();
+    let de_data: Vec<FheUint16> = bincode::deserialize_from(file).unwrap();
 
     de_data
 }
@@ -49,7 +49,7 @@ fn read_ciphertext() -> Vec<FheUint8> {
 fn main() {   
     /* generate keys and save
     let config = ConfigBuilder::all_disabled()
-        .enable_default_uint8()
+        .enable_default_uint16()
         .build();
 
     let (client_key, server_key) = generate_keys(config);
@@ -57,14 +57,14 @@ fn main() {
     let test_a = 2.3 as f64;
     let test_b = 1.2 as f64;
 
-    let clear_a = test_a.floor() as u8;
-    let clear_b = test_b.floor() as u8;
+    let clear_a = test_a.floor() as u16;
+    let clear_b = test_b.floor() as u16;
     
     let tempt = server_key.clone();
     set_server_key(tempt);
 
-    let a = FheUint8::encrypt(clear_a, &client_key);
-    let b = FheUint8::encrypt(clear_b, &client_key);
+    let a = FheUint16::encrypt(clear_a, &client_key);
+    let b = FheUint16::encrypt(clear_b, &client_key);
     let mut ciphertext = vec![a, b];
    
     let start1 = Instant::now();
@@ -95,7 +95,7 @@ fn main() {
     println!("load client key: {:?}", duration2);
 
     let start3 = Instant::now();
-    let mut new_ciphertext: Vec<FheUint8> = read_ciphertext();
+    let mut new_ciphertext: Vec<FheUint16> = read_ciphertext();
     let duration3 = start3.elapsed();
     println!("load ciphertexts: {:?}", duration3);
 
@@ -105,9 +105,9 @@ fn main() {
     let new_b = new_ciphertext[1].clone();
     let result = &new_a + &new_b;
 
-    let after_a: u8 = new_a.decrypt(&new_client_key);
-    let after_b: u8 = new_b.decrypt(&new_client_key);
-    let after_result: u8 = result.decrypt(&new_client_key);
+    let after_a: u16 = new_a.decrypt(&new_client_key);
+    let after_b: u16 = new_b.decrypt(&new_client_key);
+    let after_result: u16 = result.decrypt(&new_client_key);
 
     println!("a: {}", after_a);
     println!("b: {}", after_b);
